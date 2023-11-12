fe6214d3ec9b2502af9568525968b3b4d91495db
Fiodor Baczyński <fiodorbaczynski@gmail.com>
2023-11-12 20:36:12 +0100

    Add smooth jumping between Helix and WezTerm panes.

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -345,6 +345,8 @@ pub struct Config {
     /// Display diagnostic below the line they occur.
     pub inline_diagnostics: InlineDiagnosticsConfig,
     pub end_of_line_diagnostics: DiagnosticFilter,
+    /// Whether to jump between tmux panes when a view jump is triggered at the edge of the editor. Defaults to `false`.
+    pub jump_wezterm_panes: bool,
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, Eq, PartialOrd, Ord)]
@@ -982,6 +984,7 @@ impl Default for Config {
             jump_label_alphabet: ('a'..='z').collect(),
             inline_diagnostics: InlineDiagnosticsConfig::default(),
             end_of_line_diagnostics: DiagnosticFilter::Disable,
+            jump_wezterm_panes: false,
         }
     }
 }
@@ -1621,7 +1624,7 @@ impl Editor {
                 return;
             }
             Action::Load => {
-                let view_id = view!(self).id;
+                let view_id = view! { self }.id;
                 let doc = doc_mut!(self, &id);
                 doc.ensure_view_init(view_id);
                 doc.mark_as_focused();
@@ -1879,7 +1882,7 @@ impl Editor {
             }
         }
 
-        let view = view!(self, view_id);
+        let view = view! { self, view_id };
         let doc = doc_mut!(self, &view.doc);
         doc.mark_as_focused();
     }
@@ -1892,10 +1895,15 @@ impl Editor {
         self.focus(self.tree.prev());
     }
 
-    pub fn focus_direction(&mut self, direction: tree::Direction) {
+    pub fn focus_direction(&mut self, direction: tree::Direction) -> anyhow::Result<(), ()> {
         let current_view = self.tree.focus;
+
         if let Some(id) = self.tree.find_split_in_direction(current_view, direction) {
-            self.focus(id)
+            self.focus(id);
+
+            Ok(())
+        } else {
+            Err(())
         }
     }
 
